cb0d5222a25ba6e80dcd7eaad9119aa08ba4bbc7
Joe Baker <joesbaker@gmail.com>
2020-01-17 17:46:17 +0000

    Second commit

---
+++ docker.mk
@@ -0,0 +1,31 @@
+include .env
+
+.PHONY: up down stop prune ps shell drush logs composer
+
+default: up
+
+up:
+	@echo "Starting up containers for $(PROJECT_NAME)..."
+	docker-compose pull
+	docker-compose up -d --remove-orphans
+	@echo "Site up at http://${PROJECT_BASE_URL}"
+
+down: stop
+
+stop:
+	@echo "Stopping containers for $(PROJECT_NAME)..."
+	@docker-compose stop
+
+prune:
+	@echo "Removing containers for $(PROJECT_NAME)..."
+	@docker-compose down -v
+
+ps:
+	@docker ps --filter name='$(PROJECT_NAME)*'
+
+logs:
+	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+
+# https://stackoverflow.com/a/6273809/1826109
+%:
+	@:
